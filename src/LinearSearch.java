22c11f4f0fffd54399d2333d4f99d561030ee2a2
sumeet <sumeetchoudharymba@gmail.com>
2023-07-10 22:35:12 +0530

    Linear search

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
 public class LinearSearch {
     public static void main(String[] args) {
-int a;
+        String name="sumeet";
+        char ch='k';
+//boolean ans=findChar(name,ch);
+        System.out.println(findChar(name,ch));
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+    static boolean findChar(String str,char ch)
+    {
+        if(str.length()==0){
+            return false;
+        }
+      //  for(int i=0;i<str.length();i++){
+        //    if(ch==str.charAt(i)){
+        //        return true;
+        for(char cha:str.toCharArray()){
+            if(cha==ch){
+                return true;
+            }
+        }return false;
+
     }
 }

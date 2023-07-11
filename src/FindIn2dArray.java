a0774e66ce94d630797d5a3d8961b564a3cfa661
sumeet <sumeetchoudharymba@gmail.com>
2023-07-11 21:58:44 +0530

    find even digit

---
+++ src/FindIn2dArray.java
@@ -0,0 +1,13 @@
+public class FindIn2dArray {
+    public static void main(String[] args) {
+        int [][] arr= {
+                {22, 33, 55, 66, 99},
+                {12, 99, 878},
+                {11, 22, 888, 45}
+        };
+        int target=888;
+    }
+    static int search(int [][] arr){
+
+    }
+}
